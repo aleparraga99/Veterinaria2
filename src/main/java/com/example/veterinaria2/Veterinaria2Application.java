6bfbf589a33e5d9f5130162ee78dacc8ea7a46cc
aleparraga99 <139292133+aleparraga99@users.noreply.github.com>
2024-10-29 21:12:52 -0300

    Add files via upload

---
+++ src/main/java/com/example/veterinaria2/Veterinaria2Application.java
@@ -0,0 +1,13 @@
+package com.example.veterinaria2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Veterinaria2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Veterinaria2Application.class, args);
+	}
+
+}
